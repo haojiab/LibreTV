56f7e11f9d244d772b26c4757d6b3568da950730
haojiab <39286463+haojiab@users.noreply.github.com>
2026-01-28 14:03:50 +0800

    新增资源
    新增资源

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,8 +1,17 @@
 const CUSTOMER_SITES = {
-    qiqi: {
+    xl: {
         api: 'https://api.xinlangapi.com/xinlangapi.php/provide/vod',
         name: '新浪HTTPS',
-    }
+    },
+    ff: {
+        api: 'http://ffzy5.tv/api.php/provide/vod',
+        name: '非凡资源',
+    },
+    ff: {
+        api: 'https://wolongzyw.com/api.php/provide/vod',
+        name: '卧龙资源',
+    },
+    
 };
 
 // 调用全局方法合并
